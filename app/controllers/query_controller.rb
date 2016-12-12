c026c2f2289202f0a573969ffc2973e5a44dd529
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2016-12-12 16:33:09 -0500

    finish first two queries

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,16 +1,18 @@
 class QueryController < ApplicationController
   def election_year
   	@year = params[:year]
+
+  	# contains an array of DisplayNominee objects
     @result = Election.results_by_year(@year)
-    
+    puts @result
   end
 
   def presidential_candidates
   	@person_id = params[:candidate]
   	@candidate = Person.where(id: @person_id).first
 
+	# contains an array of DisplayNominee objects
     @result = Election.results_by_person_id(@person_id)
-    puts @result
   end
 	
   # this method doesn't require any params
